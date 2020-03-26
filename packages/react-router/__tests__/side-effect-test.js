e9ed216ff5ab2025f59d8202baf79c176291222f
Michael Jackson <michael@jackson.us>
2020-03-26 14:58:10 -0700

    Add another test to show descendant problem

--- packages/react-router/__tests__/side-effect-test.js
+++ packages/react-router/__tests__/side-effect-test.js
@@ -25,3 +25,41 @@ describe('side effect (setState) in render', () => {
     `);
   });
 });
+
+describe('side effect (setState) in render of a descendant', () => {
+  it('is not ok', () => {
+    let CounterContext = React.createContext();
+
+    function Counter() {
+      let [count, setCount] = React.useState(0);
+
+      if (count === 0) {
+        setCount(count + 1);
+      }
+
+      return (
+        <CounterContext.Provider value={[count, setCount]}>
+          <div>{count}</div>
+          <CounterChild />
+        </CounterContext.Provider>
+      );
+    }
+
+    function CounterChild({ onChange }) {
+      let [count, setCount] = React.useContext(CounterContext);
+      setCount(count + 1);
+      return null;
+    }
+
+    let renderer;
+    act(() => {
+      renderer = createTestRenderer(<Counter />);
+    });
+
+    expect(renderer.toJSON()).toMatchInlineSnapshot(`
+      <div>
+        1
+      </div>
+    `);
+  });
+});
